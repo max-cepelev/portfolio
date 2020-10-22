8f29cb5526d2f6e2cba2ddb54ac93e6c972bbcc5
max-cepelev <tsepelev_maksim@yahoo.com>
2020-10-22 17:19:01 +0500

    portfolio decoration

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,3 +16,31 @@ const counters = document.querySelectorAll('.skills__levels-percent'),
 counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+
+// Отправка данных на сервер
+function send(event, php){
+    console.log("Отправка запроса");
+    event.preventDefault ? event.preventDefault() : event.returnValue = false;
+    const req = new XMLHttpRequest();
+    req.open('POST', php, true);
+    req.onload = function() {
+        if (req.status >= 200 && req.status < 400) {
+        const json = JSON.parse(this.response); //internet explorer 11
+            console.log(json);
+            
+            // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
+            if (json.result == "success") {
+                // Если сообщение отправлено
+                alert("Сообщение отправлено");
+            } else {
+                // Если произошла ошибка
+                alert("Ошибка. Сообщение не отправлено");
+            }
+        // Если не удалось связаться с php файлом
+        } else {alert("Ошибка сервера. Номер: "+req.status);}}; 
+    
+    // Если не удалось отправить запрос. Стоит блок на хостинге
+    req.onerror = function() {alert("Ошибка отправки запроса");};
+    req.send(new FormData(event.target));
+}
